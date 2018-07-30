25211bfe7df2d50eda449ca07373c4c310af278c
despiegk <kristof@incubaid.com>
2018-07-30 12:07:07 +0200

    init

--- apps/master/blueprints/simpleblogsite/routes.py
+++ apps/master/blueprints/simpleblogsite/routes.py
@@ -4,8 +4,10 @@ from jumpscale import j
 
 login_manager = j.servers.web.latest.loader.login_manager
 
-j.tools.markdowndocs.load("https://github.com/threefoldtech//jumpscale_weblibs/tree/master/docsites_examples/simpleblogsite")
+j.tools.markdowndocs.load("https://github.com/threefoldtech/jumpscale_weblibs/tree/master/docsites_examples/simpleblogsite")
 ds = j.tools.markdowndocs.docsite_get("simpleblogsite")
+
+from IPython import embed;embed(colors='Linux')
 
 @blueprint.route('/')
 def route_default():
@@ -15,8 +17,9 @@ def route_default():
 @blueprint.route('/blog/<page>.html')
 @blueprint.route('/blog/<page>')
 def route_blog(page):
-    # doc = ds.doc_get(page,cat="blog")
-    return render_template('%s_%s.html'%(name, page))
+    
+    
+    return render_template('%s_%s.html'%(name, page),docsite=ds)
 
 
 # # ds.doc_get(template)
