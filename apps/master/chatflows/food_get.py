a02cc8c9e2a44630348d9dd0ed667f7a9b1ff32b
Abdulrahman Hamdy <arahman.hamdy91@gmail.com>
2018-07-30 17:18:31 +0200

    Enhance chatbot flow example

--- apps/master/chatflows/food_get.py
+++ apps/master/chatflows/food_get.py
@@ -1,13 +1,15 @@
-
 def chat(bot):
     """
     to call this something like localhost:3000/bot/order_my_food will be called on webserver
     """
-    
-    s = bot.string_ask("please fill in")
-    i = bot.int_ask("please fill in int")
 
-    if i == 1:
-        i+=1
-
-    bot.md_show("#header 1\n\n- %s"%i)
+    food = bot.string_ask("What do you need to eat?")
+    amount = bot.int_ask("Enter the amount you need to eat from %s in grams:" % food)
+    sides = bot.multi_choice("Choose your side dishes: ", ['rice', 'fries', 'saute', 'smash potato'])
+    print(sides)
+    drink = bot.single_choice("Choose your Drink: ", ['tea', 'coffee', 'lemon'])
+    print(drink)
+    bot.md_show("#You have ordered: `%s` grams `%s`, with sides `%s`, and `%s` drink \
+    ### Click next for the final step which will redirect you to threefold.me\
+    " % (amount, food, sides, drink))
+    bot.redirect("https://threefold.me")
