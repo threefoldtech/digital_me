ca1f35a44157fb036191fa2e1bff9ee5cfb31525
despiegk <kristof@incubaid.com>
2018-07-31 14:12:35 +0200

    initial libs

--- apps/master/chatflows/food_get.py
+++ apps/master/chatflows/food_get.py
@@ -1,15 +1,31 @@
+from jumpscale import j
+
 def chat(bot):
     """
-    to call this something like localhost:3000/bot/order_my_food will be called on webserver
+    to call http://localhost:5050/chat/session/food_get
     """
 
+    res={}
     food = bot.string_ask("What do you need to eat?")
     amount = bot.int_ask("Enter the amount you need to eat from %s in grams:" % food)
-    sides = bot.multi_choice("Choose your side dishes: ", ['rice', 'fries', 'saute', 'smash potato'])
-    print(sides)
+    sides = bot.multi_choice("Choose your side dishes: ", ['rice', 'fries', 'saute', 'smash potato'])    
     drink = bot.single_choice("Choose your Drink: ", ['tea', 'coffee', 'lemon'])
-    print(drink)
-    bot.md_show("#You have ordered: `%s` grams `%s`, with sides `%s`, and `%s` drink \
-    ### Click next for the final step which will redirect you to threefold.me\
-    " % (amount, food, sides, drink))
+
+
+    R = """
+    # You have ordered: 
+    
+    - {{amount}} grams, with sides {{sides}} and {{drink}} drink
+
+    ### Click next 
+    
+    for the final step which will redirect you to threefold.me
+
+
+    """
+
+    R2 = j.tools.jinja2.text_render(j.data.text.strip(R),**locals())
+
+    bot.md_show(R2)
+
     bot.redirect("https://threefold.me")
