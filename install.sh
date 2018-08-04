9f6090da64a4fec5ecdc69aa38ea47b212fa998c
despiegk <kristof@incubaid.com>
2018-08-04 06:22:21 +0200

    digitalme init

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 set -ex
 sudo apt install libssl-dev
 pip3 install flask_sockets html2text -y
+
+pip3 install -e .
+
+
 python3 -c "from jumpscale import j;j.servers.zdb.build()"
 python3 -c "from jumpscale import j;j.tools.jsloader.generate()"
---
+++ setup.py
@@ -0,0 +1,84 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install as _install
+from setuptools.command.develop import develop as _develop
+import os
+
+
+
+def _post_install(libname, libpath):
+    from jumpscale import j
+    # add this plugin to the config
+    c = j.core.state.configGet('plugins', defval={})
+    c[libname] = "%s/github/threefoldtech/digital_me/DigitalMeLib" % j.dirs.CODEDIR
+    # c[libname] = libpath
+    j.core.state.configSet('plugins', c)
+    j.tools.jsloader.generate()
+
+
+class install(_install):
+
+    def run(self):
+        _install.run(self)
+        libname = self.config_vars['dist_name']
+        libpath = os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), libname)
+        self.execute(_post_install, (libname, libpath),
+                     msg="Running post install task")
+
+
+class develop(_develop):
+
+    def run(self):
+        _develop.run(self)
+        libname = self.config_vars['dist_name']
+        libpath = os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), libname)
+        self.execute(_post_install, (libname, libpath),
+                     msg="Running post install task")
+
+
+long_description = ""
+try:
+    from pypandoc import convert
+    long_description = convert('README.md', 'rst')
+except ImportError:
+    long_description = ""
+
+
+setup(
+    name='DigitalMeLib',
+    version='9.4.0-rc4',
+    description='Framework behind digitalme',
+    long_description=long_description,
+    url='https://github.com/Jumpscale/lib',
+    author='ThreeFoldTech',
+    author_email='info@threefold.tech',
+    license='Apache',
+    packages=find_packages(),
+    install_requires=[
+        'Jinja2>=2.9.6',
+        'Jumpscale>=9.4.0-rc4',
+        'Jumpscale>=9.4.0-rc4',
+        'gevent>=1.2.1',
+        'grequests>=0.3.0',
+        'peewee>=2.9.2',
+        'pudb>=2017.1.2',
+        'pycapnp>=0.5.12',
+        'redis>=2.10.5',
+        'requests>=2.13.0',
+        'toml>=0.9.2',
+        'watchdog>=0.8.3',
+        'dnspython>=1.15.0',
+        'zerotier>=1.1.2',
+        'blosc>=1.5.1',
+        'pynacl>=1.1.2',
+        'ipcalc>=1.99.0',
+        'ed25519>=1.4',
+        'python-jose>=1.3.2',
+    ],
+    cmdclass={
+        'install': install,
+        'develop': develop,
+        'developement': develop,
+    },
+)
