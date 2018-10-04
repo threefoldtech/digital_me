47c6ac29b9851e6e42fc1b3bc4a873c630e2f323
Abdulrahman Hamdy <arahman.hamdy91@gmail.com>
2018-10-04 11:30:36 +0200

    update client blueprint to refresh user's JWT

--- packages/threefold/blueprints/client/routes.py
+++ packages/threefold/blueprints/client/routes.py
@@ -1,4 +1,6 @@
-import json
+from requests.exceptions import HTTPError
+from Jumpscale import j
+from jose import jwt
 from flask import redirect, session, render_template
 from blueprints.client import blueprint
 from .user import get_iyo_login_url, callback
@@ -8,6 +10,17 @@ from .user import get_iyo_login_url, callback
 def login():
     if not session.get("iyo_authenticated"):
         return redirect(get_iyo_login_url())
+
+    # Refresh jwt if expired
+    if session.get("iyo_jwt"):
+        claims = jwt.get_unverified_claims(session["iyo_jwt"])
+        if j.clients.itsyouonline.jwt_is_expired(claims['exp']):
+            try:
+                session['iyo_jwt'] = j.clients.itsyouonline.refresh_jwt_token(jwt)
+            except HTTPError:
+                session.clear()
+                return redirect(get_iyo_login_url())
+
     data = {
         "jwt": session['iyo_jwt'],
         "info": session['iyo_user_info']
