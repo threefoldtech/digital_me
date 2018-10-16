5d0680a6cdb2ea6e3c1a63ce3fade891deab84f4
Rana Tarek <rana.t.hassan@gmail.com>
2018-10-16 18:20:49 +0200

    implementing search in farmercharts

--- packages/threefold/blueprints/farmercharts/routes.py
+++ packages/threefold/blueprints/farmercharts/routes.py
@@ -4,23 +4,33 @@ from Jumpscale import j
 from collections import Counter
 
 
-# j.tools.threefold_farmer.load(reset=False)
 
+# j.tools.threefold_farmer.load(reset=False)
+client = j.servers.gedis.latest.client_get()
+node_model = j.tools.threefold_farmer.bcdb.model_get('threefold.grid.node')
 
 @blueprint.route('/node/<node_id>')
 def route_node(node_id):
     node = j.tools.threefold_farmer.bcdb.model_get('threefold.grid.node').get(int(node_id))
-    return render_template("node.html", node=node)
+    return render_template("node/node.html", node=node)
 
-@blueprint.route('/')
+@blueprint.route('/',methods = ['GET'])
 def route_default():
-    nodes = j.tools.threefold_farmer.bcdb.model_get('threefold.grid.node').get_all()
+    print(request.args.to_dict())
+    search_input = request.args.to_dict()
+    # j.shell()
+    nodes = client.farmer.node_find(**search_input).res
+    # j.shell()
     heatmap_data = Counter((node.location.latitude, node.location.longitude) for node in nodes if node.location.latitude
                            and node.location.longitude)
-    max_count = max(heatmap_data.values())
-    farmers = j.tools.threefold_farmer.bcdb.model_get('threefold.grid.farmer').get_all()
+    if heatmap_data:
+        max_count = max(heatmap_data.values())
+    else:
+        max_count = 0
+    # farmers = j.tools.threefold_farmer.bcdb.model_get('threefold.grid.farmer').get_all()
+    farmers = client.farmer.farmers_get().res
     countries = {node.location.country for node in nodes}
-    return render_template("index.html", nodes=nodes, heatmap_data=heatmap_data, max_count=max_count, farmers=farmers,
+    return render_template("node/index.html", nodes=nodes, heatmap_data=heatmap_data, max_count=max_count, farmers=farmers,
                            countries=countries)
 
 
