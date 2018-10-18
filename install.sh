a87560420d4ba9ab840ad086d4cda0d6656dacab
BolaNasr <bola.e.nasr@gmail.com>
2018-10-18 12:20:24 +0200

    test flist

--- install.sh
+++ install.sh
@@ -15,6 +15,5 @@ pip3 install flask_sockets html2text pyblake2 flask_login flask_sqlalchemy gipc
 
 pip3 install -e .
 
-
 python3 -c "from Jumpscale import j;j.servers.zdb.build()"
 js_init generate
